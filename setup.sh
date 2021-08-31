f56a33b3ad797b2724a1e40214537450c68f76e9
Nico Verbruggen <mail@nicoverbruggen.be>
2021-08-31 20:59:41 +0200

    Initial version

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "==================================================================="
+echo "This will set up your WSL for PHP 8.0 & composer (as well as nginx)!"
+echo "Installing PHP repository..."
+echo "==================================================================="
+echo ""
+
+add-apt-repository -y ppa:ondrej/php
+sudo apt update -y
+sudo apt upgrade -y
+
+echo ""
+echo "==================================================================="
+echo "Installing PHP dependencies..."
+echo "==================================================================="
+echo ""
+apt install -y ncdu curl git nginx zip php8.0 php8.0-imagick php8.0-gd php8.0-xml php8.0-mbstring php8.0-zip php8.0-mysql php8.0-sqlite3
+apt-get update && apt-get install php8.0-fpm -y
+
+echo ""
+echo "============================"
+echo "Setting up composer..."
+echo "============================"
+echo ""
+
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+php composer-setup.php --install-dir=/usr/bin --filename=composer
+
+# Ensure apache2 is removed (we're using nginx)
+apt remove apache2 -y
+apt autoremove -y
+
+echo ""
+echo "==================================================================="
+echo "WSL has been provisioned. Ready to rumble. Here's your .ini config:"
+echo "==================================================================="
+echo ""
+php --info | grep ini
